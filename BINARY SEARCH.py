871e9409b9869670ee78ed4ce70c5b8be13e056c
go-kul-anand <152949469+go-kul-anand@users.noreply.github.com>
2024-05-01 20:02:06 +0530

    Update BINARY SEARCH.py

--- BINARY SEARCH.py
+++ BINARY SEARCH.py
@@ -1,25 +1,27 @@
-def binary_search(arr, target):
-    left = 0
-    right = len(arr) - 1
-    while left <= right:
-        mid = (left + right) // 2
-        if arr[mid] == target:
+def binarySearch(array, x):
+    low = 0
+    high = len(array) - 1
+
+    # Repeat until the pointers low and high meet each other
+    while low <= high:
+        mid = low + (high - low) // 2
+
+        if array[mid] == x:
             return mid
-        elif arr[mid] > target:
-            right = mid - 1
+        elif array[mid] < x:
+            low = mid + 1
         else:
-            left = mid + 1
+            high = mid - 1
+
     return -1
 
-size = int(input("Enter the size of the array: "))
-arr = []
-for i in range(size):
-    num = int(input("Enter element " + str(i+1) + ": "))
-    arr.append(num)
 
-target = int(input("Enter a number to be searched: "))
-result = binary_search(arr, target)
+array = [3, 4, 5, 6, 7, 8, 9]
+x = 4
+
+result = binarySearch(array, x)
+
 if result != -1:
-    print("Element is present at index", str(result))
+    print("Element is present at index " + str(result))
 else:
-    print("Element is not present in array")
+    print("Not found")
